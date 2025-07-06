01a2348f01f8cbabd4057506235039185152a5a4
beedi <akshatkaushik9130@gmail.com>
2025-07-06 16:18:22 +0000

    [any message]

--- app/commands/set.go
+++ app/commands/set.go
@@ -1,6 +1,9 @@
 package commands
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
 
 type SetCommand struct{
     store *Store
@@ -12,10 +15,15 @@ func (s *SetCommand) Name() string {
 
 func (s *SetCommand) Execute(args []string) (string, error) {
     fmt.Println(args)
-    if len(args) != 2 {
-        return "", fmt.Errorf("wrong number of arguments for 'set' command")
+
+    if len(args)>2{
+        expiryMs, err := strconv.Atoi(args[3])
+        if err != nil {
+            return "", fmt.Errorf("wrong number of arguments for 'set' command")
+        }
+        s.store.SetWithExpiry(args[0], args[1], int64(expiryMs))
+        return "+OK\r\n", nil
     }
-    fmt.Println(args)
     s.store.Set(args[0], args[1])
     return "+OK\r\n", nil
 }
