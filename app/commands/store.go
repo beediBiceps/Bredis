c4216c98303795bccd19e41127065d39cb259b98
beedi <akshatkaushik9130@gmail.com>
2025-07-06 15:32:29 +0000

    [any message]

---
+++ app/commands/store.go
@@ -0,0 +1,21 @@
+package commands
+
+type Store struct{
+    store map[string]string
+}
+
+func NewStore() *Store{
+    return &Store{store:make(map[string]string)}
+}
+
+func (s *Store) Set(key string, value string) error{
+    s.store[key] = value
+    return nil
+}
+
+func (s *Store) Get(key string) (string, error){
+    if val, exists := s.store[key]; exists{
+        return val, nil
+    }
+    return "", nil
+}
